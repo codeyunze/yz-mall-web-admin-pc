72a96778297ab043b56539c8fc7027ae51de5b83
yunze <56632502+YUNZE-GH@users.noreply.github.com>
2025-03-16 16:39:47 +0800

    perf: 优化购物车生成订单展示页面

---
+++ src/views/mall/cart/utils/orderInfo.ts
@@ -0,0 +1,43 @@
+export type ReceiptInfo = {
+  /**
+   * 收货地址省编码
+   */
+  receiverProvince?: string;
+  /**
+   * 收货地址市编码
+   */
+  receiverCity?: string;
+  /**
+   * 收货地址区编码
+   */
+  receiverDistrict?: string;
+  /**
+   * 详细地址
+   */
+  receiverAddress?: string;
+  /**
+   * 收货人姓名
+   */
+  receiverName?: string;
+  /**
+   * 收货人手机号
+   */
+  receiverPhone?: string;
+  /**
+   * 收货人邮件
+   */
+  receiverEmail?: string;
+};
+
+export type ProductInfo = {
+  /** 商品Id */
+  productId: string;
+  /** 购买数量 */
+  quantity: number;
+  /** 购买的价格 */
+  price: number;
+};
+
+export type OrderBaseInfo = ReceiptInfo & {
+  products?: ProductInfo[];
+};
