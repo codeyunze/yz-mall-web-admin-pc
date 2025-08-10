c726bbdfe3ae47a4d7be3f2f8acfb72b369b972d
yunze <834363368@qq.com>
2025-08-10 12:19:51 +0800

    perf: 用户管理样式调整

--- src/views/system/user/tree.vue
+++ src/views/system/user/tree.vue
@@ -97,7 +97,7 @@ defineExpose({ onTreeReset });
   <div
     v-loading="treeLoading"
     class="h-full bg-bg_color overflow-hidden relative"
-    :style="{ minHeight: `calc(100vh - 141px)` }"
+    :style="{ minHeight: `calc(100vh - 146px)` }"
   >
     <div class="flex items-center h-[34px]">
       <el-input
